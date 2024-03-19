0506217d306feb0577458227f52b04737217c15c
Kara Alexandra <ardnaxelarak@gmail.com>
2024-03-19 23:36:46 +0000

    OH NO SERVING DEV AND PROD FROM SAME DIR WAS MISTAKE

--- server.js
+++ server.js
@@ -207,7 +207,7 @@ app.get("/myitems/:channel", authViewer);
 app.get("/myzora/edit", authChannel);
 
 app.use(history());
-app.use(express.static(process.env.DIST_DIR));
+app.use(express.static("dist"));
 
 const clients = [];
 
